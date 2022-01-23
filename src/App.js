c4555b654c55fb99308dbff7a05cb1f0262cf8e4
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-01-23 17:01:12 -0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 import './style/css/index.css';
+import './style/css/animation-routers.css'
 import Header from './components/Header.js'
 import Home from './pages/Home.js';
 import Destination from './pages/Destination.js';
@@ -8,27 +10,29 @@ import Crew from './pages/Crew.js';
 import Technology from './pages/Technology.js';
 
 function App() {
-
+  
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+  
+        <BrowserRouter>
+          <div className="Container">
+        
+          <Header />
+            <TransitionGroup>
+              <CSSTransition>
+                <Routes>
+                  <Route path="/" element={<Home />}/>
+                  <Route path="/destination" element={<Destination />} />
+                  <Route path="/crew" element={<Crew />} />
+                  <Route path="/technology" element={<Technology />} />
+                  <Route path="*" element={<Home />}/>
+                </Routes>
+              </CSSTransition>
+            </TransitionGroup>
 
-      <main>
-        <div className="Container">
-      
-        <Header />
-
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
-          <Route path="*" element={<Home />}/>
-        </Routes>
-      
-        </div>
-      </main>
-
-    </BrowserRouter>
+          </div>
+        </BrowserRouter>
+    
+ 
   );
   
 }
